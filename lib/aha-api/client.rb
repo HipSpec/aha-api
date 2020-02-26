bb2991e81c6a7ce8bb85ded6500367cf2f79305e
Mason Hensley <mason@hipspec.com>
2020-02-26 14:47:15 -0600

    updating features queries for product and release scoping

--- lib/aha-api/client.rb
+++ lib/aha-api/client.rb
@@ -1,20 +1,20 @@
-require 'aha-api/version'
+require "aha-api/version"
 
-require 'aha-api/configuration'
-require 'aha-api/connection'
-require 'aha-api/request'
+require "aha-api/configuration"
+require "aha-api/connection"
+require "aha-api/request"
 
-require 'aha-api/resources/features'
-require 'aha-api/resources/ideas'
-require 'aha-api/resources/comments'
-require 'aha-api/resources/integration_fields'
-require 'aha-api/resources/integration_candidates'
+require "aha-api/resources/features"
+require "aha-api/resources/ideas"
+require "aha-api/resources/comments"
+require "aha-api/resources/integration_fields"
+require "aha-api/resources/integration_candidates"
 
 module AhaApi
   class Client
     attr_accessor(*Configuration::VALID_OPTIONS_KEYS)
 
-    def initialize(options={})
+    def initialize(options = {})
       Configuration::VALID_OPTIONS_KEYS.each do |key|
         AhaApi.send("#{key}=", options[key]) if options[key]
         send("#{key}=", AhaApi.send(key))
--- lib/aha-api/resources/features.rb
+++ lib/aha-api/resources/features.rb
@@ -1,17 +1,45 @@
 module AhaApi
   class Resource
     module Features
-
-      def create_feature(name, description, options={})
-        post("api/#{api_version}/features", options.merge({:name => name, :description => description}))
+      def create_feature(name, description, options = {})
+        post("api/#{api_version}/features", options.merge({ :name => name, :description => description }))
       end
 
-      def feature(ref, options={})
+      # client.features(options = {})
+      # https://www.aha.io/api/resources/features#anchor-resource-features-example-get_all_features
+      def features(options = {})
+        get("api/#{api_version}/features", options)
+      end
+
+      def feature(ref, options = {})
         get("api/#{api_version}/features/#{ref}", options)
       end
-      
+
       def update_feature(ref, options = {})
         put("api/#{api_version}/features/#{ref}", options)
+      end
+
+      # client.create_feature_in_release('name', 'description', 'PROD-R-1', options = {})
+      # https://www.aha.io/api/resources/features#anchor-resource-features-example-create_a_feature
+      def create_feature_in_release(name, description, release, options = {})
+        post("api/#{api_version}/releases/#{release}/features", options.merge({ :name => name, :description => description }))
+      end
+
+      # client.features_in_release('DEMO-R-3')
+      # https://www.aha.io/api/resources/features#anchor-resource-features-example-create_a_feature_in_the_default_release
+      def features_in_release(release, options = {})
+        get("api/#{api_version}/releases/#{release}/features", options)
+      end
+
+      # client.create_feature_in_product('name', 'description', 'PROD', options = {})
+      def create_feature_in_product(name, description, product, options = {})
+        post("api/#{api_version}/releases/#{product}/features", options.merge({ :name => name, :description => description }))
+      end
+
+      # client.features_in_product('DEMO-R-3')
+      # https://www.aha.io/api/resources/features#anchor-resource-features-example-get_all_features_for_a_product
+      def features_in_product(product, options = {})
+        get("api/#{api_version}/products/#{release}/features", options)
       end
     end
   end
